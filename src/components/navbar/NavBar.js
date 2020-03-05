4d2202e16b71adb199fdcfe260394635d981a8f1
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-03-05 16:43:24 -0600

    fixed link errors

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -3,83 +3,62 @@ import { Menu, Segment, Button } from "semantic-ui-react";
 import { Link, withRouter } from "react-router-dom";
 import "./NavBar.css";
 
-const NavBar = ({}) => {
+const NavBar = ({ history }) => {
   const [activeItem, setActiveItem] = useState("home");
 
-  const handleItemClick = (e, { name }) => setActiveItem(name);
-
+  const handleItemClick = (e, { name }) => {
+    history.push(`/${name}`);
+    setActiveItem(name);
+  };
 
   return (
     <Segment inverted id="navbar-container">
       <Menu inverted pointing secondary>
-        <Menu.Item>
-            <img src="https://emoji.slack-edge.com/T03F2SDTJ/handyandy/8378d8b808441ab4.jpg" />
-          <Link className="nav-link" to="/">
-            <h1>Handy Andy</h1>
-          </Link>
+        <Menu.Item name="" onClick={handleItemClick}>
+          <img src="https://emoji.slack-edge.com/T03F2SDTJ/handyandy/8378d8b808441ab4.jpg" />
+          <h1>Handy Andy</h1>
         </Menu.Item>
         <Menu.Item
           name="news"
           active={activeItem === "news"}
           onClick={handleItemClick}
         >
-          <Link className="nav-link" to="/news">
-            News
-          </Link>
+          News
         </Menu.Item>
         <Menu.Item
           name="messages"
           active={activeItem === "messages"}
           onClick={handleItemClick}
         >
-          <Link
-            className="nav-link"
-            to="/messages"
-          >
-            Messages
-          </Link>
+          Messages
         </Menu.Item>
         <Menu.Item
           name="friends"
           active={activeItem === "friends"}
           onClick={handleItemClick}
         >
-          <Link
-            className="nav-link"
-            to="/friends"
-          >
-            Friends
-          </Link>
+          Friends
         </Menu.Item>
         <Menu.Item
           name="tasks"
           active={activeItem === "tasks"}
           onClick={handleItemClick}
         >
-          <Link className="nav-link" to="/tasks">
-            Tasks
-          </Link>
+          Tasks
         </Menu.Item>
         <Menu.Item
           name="events"
           active={activeItem === "events"}
           onClick={handleItemClick}
         >
-          <Link className="nav-link" to="/events" >
-            Events
-          </Link>
+          Events
         </Menu.Item>
       </Menu>
       <div>
-        <Button
-          circular
-          id="user-icon"
-          disabled
-          icon="user circle outline"
-        />
+        <Button circular id="user-icon" disabled icon="user circle outline" />
       </div>
     </Segment>
   );
 };
 
-export default NavBar;
+export default withRouter(NavBar);
