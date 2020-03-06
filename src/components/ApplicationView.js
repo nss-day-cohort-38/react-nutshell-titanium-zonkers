008bbfc220811591968bf802a1fc6b747db7727a
Jeremy Mattingly <jamattingly@live.com>
2020-03-06 16:07:43 -0600

    added switch in appviews

--- src/components/ApplicationView.js
+++ src/components/ApplicationView.js
@@ -1,30 +1,34 @@
 import React from "react";
-import { Route, Redirect} from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import "./ApplicationView.css"
 import EventList from "./events/EventList"
 import LoginPage from "./auth/Auth"
+import Home from "./home/Home"
 
 const ApplicationViews = () => {
   return (
     <div id="application-views-container">
-      <Route
-        exact
-        path="/"
-        render={props =>
-          sessionStorage.getItem('userId') === null
-            ? <LoginPage {...props}/>
-            : <h1>This is Home</h1>
-        }
-      />
-      <Route
-        exact
-        path="/events"
-        render={props =>
-          sessionStorage.getItem('userId') === null
-          ? <Redirect exact to="/"/>
-          : <EventList />
-        }
-      />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={props =>
+            sessionStorage.getItem('userId') === null
+              ? <LoginPage {...props} />
+              : <Home />
+          }
+        />
+        <Route
+          exact
+          path="/events"
+          render={props =>
+            sessionStorage.getItem('userId') === null
+              ? <Redirect exact to="/" />
+              : <EventList />
+          }
+        />
+        <Route component={Home} />
+      </Switch>
     </div>
   );
 };
