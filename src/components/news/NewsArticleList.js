007a2c18f7e9300aa8ee01e1f04f617102d3fe8e
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-03-07 10:23:54 -0600

    added styling to add news button

--- src/components/news/NewsArticleList.js
+++ src/components/news/NewsArticleList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import NewsArticleCard from './NewsArticleCard';
 import dbAPI from '../../modules/dbAPI';
+import { Button } from "semantic-ui-react";
 
 const NewsArticleList = (props) => {
     const [newsArticles, setNewsArticles] = useState([]);
@@ -53,13 +54,11 @@ const NewsArticleList = (props) => {
             <>
                 <section className="news-content-container">
                     <div className="add-news-button-container">
-                        <button
-                            type="button"
-                            className="add-news-button"
+                        <Button
                             onClick={() => {
                                 props.history.push("/newsArticles/new")
                             }}
-                        >Add News Article</button>
+                        >Add News Article</Button>
                     </div>
                     <div className="news-cards-container">
                         {newsArticles.map(newsArticle =>
@@ -79,3 +78,90 @@ const NewsArticleList = (props) => {
 
 export default NewsArticleList;
 
+
+
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import NewsArticleCard from './NewsArticleCard';
+// import dbAPI from '../../modules/dbAPI';
+// import { Button } from "semantic-ui-react";
+
+// const NewsArticleList = (props) => {
+//     const [newsArticles, setNewsArticles] = useState([]);
+//     const [isLoading, setIsLoading] = useState(false);
+
+//     const activeUserId = parseInt(sessionStorage.getItem("userId"));
+
+//     const getNewsArticles = () => {
+//         return dbAPI.getObjectByResource("newsArticles", activeUserId).then(newsFromAPI => {
+//             const sortedNewsFromAPI = newsFromAPI.sort((a,b) => {
+//                 return new Date(b.created_at) - new Date(a.created_at)
+//             })
+//             setNewsArticles(sortedNewsFromAPI)
+//         })
+//     }
+
+//     const handleDeleteNewsArticle = (id) => {
+//         if (window.confirm("Are you sure you want to delete this news article?")) {
+//             setIsLoading(true);
+//             dbAPI.deleteObjectByResource("newsArticles", id)
+//                 .then(() => dbAPI.getObjectByResource("newsArticles", activeUserId).then(setNewsArticles))
+//         }
+//     }
+
+//     useEffect(() => {
+//         getNewsArticles();
+//     }, []);
+
+//     if (newsArticles.length === 0) {
+//         return (
+//             <>
+//                 <section className="news-content-container">
+//                     <div className="add-news-button-container">
+//                         <button
+//                             type="button"
+//                             className="add-news-button"
+//                             onClick={() => {
+//                                 props.history.push("/newsArticles/new")
+//                             }}
+//                         >Add News Article</button>
+//                     </div>
+//                     <div className="no-news-message-container">
+//                         <h1 className="no-news-message">You have no saved news articles.</h1>
+//                     </div>
+//                 </section>
+//             </>
+//         )
+//     } else {
+//         return (
+//             <>
+//                 <section className="news-content-container">
+//                     <div className="add-news-button-container">
+//                         <button
+//                             type="button"
+//                             className="add-news-button"
+//                             onClick={() => {
+//                                 props.history.push("/newsArticles/new")
+//                             }}
+//                         >Add News Article</button>
+//                     </div>
+//                     <div className="news-cards-container">
+//                         {newsArticles.map(newsArticle =>
+//                             <NewsArticleCard
+//                                 key={newsArticle.id}
+//                                 newsArticle={newsArticle}
+//                                 handleDeleteNewsArticle={handleDeleteNewsArticle}
+//                                 disabled={isLoading}
+//                                 {...props}
+//                             />)}
+//                     </div>
+//                 </section>
+//             </>
+//         )
+//     }
+// }
+
+// export default NewsArticleList;
