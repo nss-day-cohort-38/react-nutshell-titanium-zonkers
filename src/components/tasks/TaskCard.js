85ba654f65daeca310f7c144b295f40e38f25e78
Jeremy Mattingly <jamattingly@live.com>
2020-03-09 11:58:25 -0500

    Tasks edit functioning

--- src/components/tasks/TaskCard.js
+++ src/components/tasks/TaskCard.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { List } from "semantic-ui-react"
 import moment from 'moment'
 import dbAPI from "../../modules/dbAPI";
@@ -7,6 +7,8 @@ import dbAPI from "../../modules/dbAPI";
 
 
 export default ({ taskObj, changeComplete }) => {
+    
+    const [ editTitle, setEditTitle ] = useState(false)
 
     async function toggleComplete () {
         const toggledCompletion = { "is_complete": !taskObj.is_complete }
@@ -23,7 +25,20 @@ export default ({ taskObj, changeComplete }) => {
         }
     }
 
+    async function saveEditedTask(event) {
+        if (event.key === 'Enter') {
+           await dbAPI.patchObjectByResource('tasks', taskObj.id, {"title": event.target.value })
+                .then(setEditTitle(false))
+                .then(changeComplete(true))
+
+        }
+    }
+
     const convertedTaskDate = moment(taskObj.completion_date).format('LLL')
+
+    useEffect(()=>{
+        displayTaskBooleans()
+    }, [editTitle])
     
     const iconBoolean = () => {
         if(taskObj.is_complete === true) {
@@ -32,21 +47,36 @@ export default ({ taskObj, changeComplete }) => {
             return(<List.Icon className='xMark' name='x' onClick={toggleComplete}/>)
         };
     };
+
+    const toggleEdit = (event) => {
+        setEditTitle(!editTitle)
+    }
     
-    const strikeThroughBoolean = () => {
+    const displayTaskBooleans = () => {
         if(taskObj.is_complete === true) {
             return(
             <List.Content className='completed'>
-                <List.Header as='a'>{taskObj.title}</List.Header>
+                <List.Header id={`$task-${taskObj.id}`} as='a'>{taskObj.title}</List.Header>
                 <List.Description>
                 {convertedTaskDate}
                 </List.Description>
             </List.Content>
                 );
-        } else if (taskObj.is_complete === false) {
+        } else if (taskObj.is_complete === false && editTitle === true) {
+            return(
+                <List.Content className='incomplete'>
+                    <input id={`input-${taskObj.id}`} placeholder={`${taskObj.title}`} onKeyUp={saveEditedTask} />
+                    <List.Description>
+                    {convertedTaskDate}
+                    </List.Description>
+                </List.Content>
+            );
+        }
+        
+        else if (taskObj.is_complete === false) {
             return(
             <List.Content className='incomplete'>
-                <List.Header as='a'>{taskObj.title}</List.Header>
+                <List.Header id={`task-${taskObj.id}`} as='a' onDoubleClick={toggleEdit}>{taskObj.title}</List.Header>
                 <List.Description>
                 {convertedTaskDate}
                 </List.Description>
@@ -59,7 +89,7 @@ export default ({ taskObj, changeComplete }) => {
         <List.Item className="taskDiv" >
             <List.Icon className='deleteTask' name='trash alternate' onClick={deleteTask}/>
             {iconBoolean()}
-            {strikeThroughBoolean()}
+            {displayTaskBooleans()}
         </List.Item>
         
     );
