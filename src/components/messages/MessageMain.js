8770830d3a0190fd3012cd47ec9650f00534dc3f
Jeremy Mattingly <jamattingly@live.com>
2020-03-09 15:36:39 -0500

    More files for messages

--- src/components/messages/MessageMain.js
+++ src/components/messages/MessageMain.js
@@ -1,15 +1,34 @@
 import React from "react"
-
+import { Grid, Image, Button } from "semantic-ui-react"
+import MessageList from "./MessageList"
+import "./Messages.css"
 
 
 
 const MessagesMain = () => {
     return (
-        <h1>Message someday!</h1>
-    )
-}
-
-
-
-
+        <Grid celled className="messagesContainer">
+            <Grid.Row>
+                <Grid.Column width={12}>
+                    <MessageList />
+                </Grid.Column>
+                <Grid.Column width={4}>
+                    <Image src='/images/wireframe/centered-paragraph.png' />
+                </Grid.Column>
+            </Grid.Row> 
+            <Grid.Row height={1} id='messagesBottomRow'>
+                <Grid.Column width={12}>
+                    <Button>Send Message</Button>
+                </Grid.Column>
+                <Grid.Column width={4}>
+                    <Button>Toggle Friends</Button>
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
+            )
+        }
+        
+        
+        
+        
 export default MessagesMain;
