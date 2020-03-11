4f084c5d394b0a07b08227de2cb3c1433ab34e09
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-03-11 10:52:59 -0500

    added simple css to settings

--- src/components/settings/SettingsCard.js
+++ src/components/settings/SettingsCard.js
@@ -208,7 +208,7 @@ const SettingsCard = (props) => {
                     user={props.user}
                     className="settings-container">
                     <Card.Content className="settings-content">
-                        <Image src={userInfo.image} />
+                        <Image className="user-image" src={userInfo.image} />
                         <Card.Header className="settings-username">Username: {userInfo.username}</Card.Header>
                         <Button size="tiny"
                             onClick={toggleUsernameModal}
